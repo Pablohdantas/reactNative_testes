2d5ae680b810339209cd190c6288f39ccfbe1e71
Pablo Henrique <pablo.hdantass@gmail.com>
2024-06-15 15:45:12 -0300

    feat: Tela de dados adicionada

--- App.tsx
+++ App.tsx
@@ -1,17 +1,27 @@
 import Home from "./src/Screens/Home";
-import Details from "./src/Screens/Details";
+import Dados from "./src/Screens/Dados";
+import InformarDados from "./src/Screens/InformarDados";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="home" component={Home}/>
-        <Stack.Screen name="details" component={Details}/>
-      </Stack.Navigator>
+      <BottomTab.Navigator 
+      screenOptions={{tabBarStyle:{
+        backgroundColor:"#000000",
+        borderTopWidth:0,
+        height:80,
+        bottom:10,
+        position:"absolute"
+      }}}
+      >
+        <BottomTab.Screen name="Home" component={Home}/>
+        <BottomTab.Screen name="Dados" component={Dados}/>
+        <BottomTab.Screen name="Informar dados" component={InformarDados}/>
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,25 +1,12 @@
-import { StyleSheet, Text, View,TextInput,TouchableOpacity} from 'react-native';
+import { StyleSheet, Text, View,TextInput,TouchableOpacity, ScrollView} from 'react-native';
 
-export default function Home({navigation}:any) {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <View style={styles.containerFormulario}>
-        <Text style={styles.containerFormularioTitulo}>Formulario para Obter informações</Text>
-        <View style={styles.containerFormularioGeral}>
-          <Text style={styles.containerFormularioTexto}>Nome:</Text>
-          <TextInput style={styles.containerFormularioInputs} placeholder='Informe seu nome...' placeholderTextColor={"#696969"}>
-          </TextInput>
-          <Text style={styles.containerFormularioTexto}>Idade:</Text>
-          <TextInput style={styles.containerFormularioInputs} placeholder='Informe sua idade...' placeholderTextColor={"#696969"}>
-          </TextInput>
-          <TouchableOpacity style={styles.containerFormularioGeralBotao}>
-            <Text style={{color:"#ffffff"}}>Enviar</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.containerFormularioGeralBotao} onPress={()=>navigation.navigate("details")}>
-            <Text style={{color:"#ffffff"}}>Ir para pagina Details</Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <ScrollView style ={{width:"90%"}} >
+     
+      <Text style={{color:"#ffffff"}}>Aqui é a pagina Home</Text>
+    </ScrollView>
     </View>
   );
 }
@@ -30,42 +17,6 @@ const styles = StyleSheet.create({
     backgroundColor:"#000000",
     borderWidth:2,
     borderColor:"#ffffff",
-    justifyContent:"center",
-    alignItems:"center",
-  },
-  containerFormulario:{
-    padding:10, 
-    paddingTop:30,
-    borderWidth:2,
-    borderColor:"#ffffff",
-    gap:100,
-    alignItems:"center",
-    justifyContent:"center",
-  },
-  containerFormularioTexto:{
-    color:"#ffffff",
-  },
-  containerFormularioTitulo:{
-    color:"#ffffff",
-    fontSize:20,
-  },
-  containerFormularioInputs:{
-    width:300,
-    padding:10,
-    height:50,
-    borderWidth:2,
-    color:"#ffffff",
-    borderColor:"#ffffff",
-  },
-  containerFormularioGeral:{
-    gap:10
-  },
-  containerFormularioGeralBotao:{
-    width:300,
-    height:50,
-    borderWidth:2,
-    borderColor:"#ffffff",
-    marginBottom:30,
     justifyContent:"center",
     alignItems:"center",
   },
